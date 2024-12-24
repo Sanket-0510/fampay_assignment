13c571e45bd228eb779a85ef3a037b765089d751
Sanket Teli <telisanket2002@gmail.com>
2024-12-25 02:31:50 +0530

    added the url path for the video searching

--- fampay/server/urls.py
+++ fampay/server/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
 	path('getVideos', views.PaginatedVideoListView.as_view(), name='paginated_videos'),
+    path('serchVideos', views.OptimizedSearchVideo.as_view(), name="view_for_searching_videos")
 ]
 
 
