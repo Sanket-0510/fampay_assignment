06c592d42492c12e55222d1a697c0cc3f2de9de4
Sanket Teli <telisanket2002@gmail.com>
2024-12-25 02:24:35 +0530

    added the view for the searchAPI

--- fampay/server/views.py
+++ fampay/server/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView, status
 from .models import Video, APIkey
 from .serializers import VideoSerializer, APIKeySerializer
+from django.db.models import Q
 
 class PaginatedVideoListView(APIView):
     """API to return videos in a paginated response sorted by publish datetime."""
@@ -14,10 +15,27 @@ class PaginatedVideoListView(APIView):
         serializer = VideoSerializer(result_page, many=True)
         return paginator.get_paginated_response(serializer.data)
 
+    
+class OptimizedSearchVideo(APIView):
+    """API for searching videos by title or description with partial matching."""
+
+    def get(self, request):
+        search_query = request.query_params.get('query', '')
+
+        if search_query:
+            videos = Video.objects.filter(
+                Q(video_title__icontains=search_query) | Q(video_description__icontains=search_query)
+            ).order_by('-pub_date') 
+        else:
+            videos = Video.objects.all().order_by('-pub_date')
+        serializer = VideoSerializer(videos, many=True)
+        return Response({'status': 'success', 'data': serializer.data})
+    
+
 
 class APIKeyView(APIView):
     """API to add and view API keys."""
-    
+
     def post(self, request):
         """Add a new API key."""
         serializer = APIKeySerializer(data=request.data)
